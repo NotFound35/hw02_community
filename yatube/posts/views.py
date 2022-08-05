a2de889401bd4489b36451521dd69250725b3f49
Александр Романов <roman.ulbricht@yandex.ru>
2022-08-05 21:04:48 +0300

    ласт шанс

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -16,7 +16,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group)[:10]
+    posts = Post.objects.filter(group=group)[:post_count]
     context = {
         "group": group,
         "posts": posts,
